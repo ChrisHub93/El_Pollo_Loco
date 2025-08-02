e0b70c69ab21bd4636dacede7ddf9e1e18780fde
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-08-02 15:02:23 +0200

    animate character

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,17 +1,30 @@
 class Character extends MoveableObject {
   height = 280;
   y = 155;
+  IMAGE_WALKING = [
+    "../assets/img/2_character_pepe/2_walk/W-21.png",
+    "../assets/img/2_character_pepe/2_walk/W-22.png",
+    "../assets/img/2_character_pepe/2_walk/W-23.png",
+    "../assets/img/2_character_pepe/2_walk/W-24.png",
+    "../assets/img/2_character_pepe/2_walk/W-25.png",
+    "../assets/img/2_character_pepe/2_walk/W-26.png",
+  ];
+  currentImage = 0;
 
   constructor() {
     super().loadImage("../assets/img/2_character_pepe/1_idle/idle/I-1.png");
-    this.loadImages([
-      "../assets/img/2_character_pepe/2_walk/W-21.png",
-      "../assets/img/2_character_pepe/2_walk/W-22.png",
-      "../assets/img/2_character_pepe/2_walk/W-23.png",
-      "../assets/img/2_character_pepe/2_walk/W-24.png",
-      "../assets/img/2_character_pepe/2_walk/W-25.png",
-      "../assets/img/2_character_pepe/2_walk/W-26.png"
-    ]);
+    this.loadImages(this.IMAGE_WALKING);
+
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      let i = this.currentImage % this.IMAGE_WALKING.length;
+      let path = this.IMAGE_WALKING[i];
+      this.img = this.imageCache[path];
+      this.currentImage++;
+    }, 100);
   }
   jump() {}
 }
