7d29cabb6c7a87155a0299d5087c2334353e2499
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-08-02 11:15:59 +0200

    requestAnimationFrame()

--- classes/world.class.js
+++ classes/world.class.js
@@ -2,19 +2,39 @@ class World {
   character = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken()];
   ctx;
+  canvas;
 
   constructor(canvas) {
     this.ctx = canvas.getContext("2d");
+    this.canvas = canvas;
     this.draw();
   }
 
   draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
     this.ctx.drawImage(
       this.character["img"],
       this.character["x"],
       this.character["y"],
-      this.character['width'],
-      this.character["height"],
+      this.character["width"],
+      this.character["height"]
     );
+
+    this.enemies.forEach((emeny) => {
+      this.ctx.drawImage(
+        emeny["img"],
+        emeny["x"],
+        emeny["y"],
+        emeny["width"],
+        emeny["height"]
+      );
+    });
+
+    // draw() wird immer wieder aufgerufen.
+    let self = this;
+    requestAnimationFrame(function () {
+      self.draw();
+    });
   }
 }
