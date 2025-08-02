ce2a9664d4761cc0dd156f4250f6eabdff2eeea7
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-08-02 10:58:34 +0200

    Show character

--- scripts/game.js
+++ scripts/game.js
@@ -1,9 +1,9 @@
 let canvas;
-let ctx;
-let world = new World();
+let world;
 
 function initIndex() {
   canvas = document.getElementById("canvas");
+  world = new World(canvas)
 
   console.log("My Character is", world['character']);
 }
