b59172ea3d816becbf3b83f0c2b62331825f7de3
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-08-04 10:30:42 +0200

    Charakter is movable now

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,6 +1,7 @@
 class Character extends MoveableObject {
   height = 280;
   y = 155;
+  speed = 10;
   IMAGE_WALKING = [
     "../assets/img/2_character_pepe/2_walk/W-21.png",
     "../assets/img/2_character_pepe/2_walk/W-22.png",
@@ -20,7 +21,16 @@ class Character extends MoveableObject {
 
   animate() {
     setInterval(() => {
-      if (this.world.keyboard.RIGHT) {
+        if (this.world.keyboard.RIGHT) {
+            this.x += this.speed;
+        }
+        if (this.world.keyboard.LEFT) {
+            this.x -= this.speed;
+        }
+    }, 1000 / 60);
+
+    setInterval(() => {
+      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
         let i = this.currentImage % this.IMAGE_WALKING.length;
         let path = this.IMAGE_WALKING[i];
         this.img = this.imageCache[path];
