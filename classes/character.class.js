e765d4d7f9970f01dbc87388bd43a1a43b1e12c5
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-08-05 11:07:18 +0200

    Bugfix: character can`t outrun the map now. + Code optimation

--- classes/character.class.js
+++ classes/character.class.js
@@ -21,19 +21,19 @@ class Character extends MoveableObject {
 
   animate() {
     setInterval(() => {
-      if (this.world.keyboard.RIGHT) {
+      if (this.world["keyboard"]["RIGHT"] && this.x < this.world["level"]["level_end_x"]) {
         this.x += this.speed;
         this.otherDirection = false;
       }
-      if (this.world.keyboard.LEFT) {
+      if (this.world["keyboard"]["LEFT"] && this.x > 0) {
         this.x -= this.speed;
         this.otherDirection = true;
       }
-      this.world.camera_x = -this.x;
+      this.world["camera_x"] = -this.x + 100;
     }, 1000 / 60);
 
     setInterval(() => {
-      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+      if (this.world["keyboard"]["RIGHT"] || this.world["keyboard"]["LEFT"]) {
         let i = this.currentImage % this.IMAGE_WALKING.length;
         let path = this.IMAGE_WALKING[i];
         this.img = this.imageCache[path];
