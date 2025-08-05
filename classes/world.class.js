b6b406ae46f41b3b19d40a02bfbdd60191eef530
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-08-05 09:28:03 +0200

    Added camera movemend

--- classes/world.class.js
+++ classes/world.class.js
@@ -20,6 +20,7 @@ class World {
   ctx;
   canvas;
   keyboard;
+  camera_x = 0;
 
   constructor(canvas, keyboard) {
     this.ctx = canvas.getContext("2d");
@@ -36,10 +37,12 @@ class World {
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+    this.ctx.translate(this.camera_x, 0);
     this.addObjectsToMap(this.backgroundObjects);
     this.addObjectsToMap(this.clouds);
     this.addObjectsToMap(this.enemies);
     this.addToMap(this.character);
+    this.ctx.translate(- this.camera_x, 0);
 
     // Dadurch draw() wird immer wieder aufgerufen ->
     let self = this;
