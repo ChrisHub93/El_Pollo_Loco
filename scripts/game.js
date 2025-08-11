83406f39d3a5c7861f1968473494e4fb949bb3d7
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-08-11 15:28:32 +0200

    Added fullscren function

--- scripts/game.js
+++ scripts/game.js
@@ -58,3 +58,26 @@ window.addEventListener("keyup", (e) => {
     keyboard.D = false;
   }
 });
+
+function fullscreen() {
+  let fullscreenRef = document.getElementById("fullscreen");
+  enterFullscreen(fullscreenRef);
+}
+
+function enterFullscreen(element) {
+  if(element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if(element.msRequestFullscreen) {      // for IE11 (remove June 15, 2022)
+    element.msRequestFullscreen();
+  } else if(element.webkitRequestFullscreen) {  // iOS Safari
+    element.webkitRequestFullscreen();
+  }
+}
+
+function exitFullscreen() {
+  if(document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if(document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+}
