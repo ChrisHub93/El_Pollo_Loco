40a89ab0afffdab25bb5019264efefe8b0172307
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-08-14 10:51:54 +0200

    Bottle rotate while flying now

--- classes/throwable-object.class.js
+++ classes/throwable-object.class.js
@@ -2,14 +2,32 @@ class ThrowableObject extends MoveableObject {
   speedY = 30;
   speedX = 20;
 
+  IMAGES_BOTTLE_ROTATION = [
+    "../assets/img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
+    "../assets/img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
+    "../assets/img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png",
+    "../assets/img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png",
+  ];
+
+  IMAGES_BOTTLE_SPLASH = [
+    "../assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png",
+    "../assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png",
+    "../assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png",
+    "../assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png",
+    "../assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png",
+    "../assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png",
+  ];
+
   constructor(x, y) {
     super().loadImage(
       "../assets/img/7_statusbars/3_icons/icon_salsa_bottle.png"
     );
+    this.loadImages(this.IMAGES_BOTTLE_ROTATION);
+    this.loadImages(this.IMAGES_BOTTLE_SPLASH);
     this.x = x;
     this.y = y;
-    this.width = 50;
-    this.height = 60;
+    this.width = 80;
+    this.height = 90;
     this.throw(); // Zum testen!
   }
 
@@ -17,6 +35,7 @@ class ThrowableObject extends MoveableObject {
     this.applyGravity();
     setInterval(() => {
       this.x += 10;
+      this.playAnimation(this.IMAGES_BOTTLE_ROTATION);
     }, 25);
   }
 }
