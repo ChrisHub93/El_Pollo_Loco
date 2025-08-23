09ab897e884392370ac56c0a8e3b8342210bbee2
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-08-23 15:11:15 +0200

    Code cleanup for keyboard.class

--- classes/keyboard.class.js
+++ classes/keyboard.class.js
@@ -1,9 +1,62 @@
 class Keyboard {
+  constructor() {
+    this.keyboardReady = true;
+    this.W = false;
+    this.A = false;
+    this.D = false;
+    this.SPACE = false;
+    this.IDLE = false;
+    this.LONG_IDLE = false;
+    this.pressed = false;
 
-  keyboardReady = true;
-  W = false;
-  A = false;
-  D = false;
-  SPACE = false;
-  pressed;
+    this.idleTimeout = null;
+    this.longIdleTimeout = null;
+
+    // Event Listener direkt in der Klasse binden
+    window.addEventListener("keydown", (e) => this.handleKey(e, true));
+    window.addEventListener("keyup", (e) => this.handleKey(e, false));
+  }
+
+  handleKey(e, isPressed) {
+    if (e.keyCode == 87) this.setKey("W", isPressed);
+    if (e.keyCode == 65) this.setKey("A", isPressed);
+    if (e.keyCode == 68) this.setKey("D", isPressed);
+    if (e.keyCode === 32) this.setKey("SPACE", isPressed);
+  }
+
+  setKey(key, isPressed) {
+    this[key] = isPressed;
+
+    if (isPressed) {
+      this.pressed = true;
+      this.IDLE = false;
+      this.LONG_IDLE = false;
+
+      clearTimeout(this.idleTimeout);
+      clearTimeout(this.longIdleTimeout);
+
+      this.idleTimeout = setTimeout(() => {
+        this.IDLE = true;
+      }, 1000);
+
+      this.longIdleTimeout = setTimeout(() => {
+        this.LONG_IDLE = true;
+      }, 5000);
+    } else {
+      if (!this.W && !this.A && !this.D && !this.SPACE) {
+        this.pressed = false;
+
+        clearTimeout(this.idleTimeout);
+        clearTimeout(this.longIdleTimeout);
+
+        this.idleTimeout = setTimeout(() => {
+          this.IDLE = true;
+        }, 1000);
+
+        this.longIdleTimeout = setTimeout(() => {
+          this.LONG_IDLE = true;
+        }, 5000);
+      }
+    }
+  }
 }
--- scripts/game.js
+++ scripts/game.js
@@ -9,21 +9,6 @@ function initIndex() {
   console.log("My Character is", world["character"]);
 }
 
-window.addEventListener("keydown", (e) => {
-  if (e.keyCode == 87) keyboard.W = true;
-  if (e.keyCode == 65) keyboard.A = true;  
-  if (e.keyCode == 68) keyboard.D = true;  
-  if (e.keyCode === 32) keyboard.SPACE = true; 
-
-});
-
-window.addEventListener("keyup", (e) => {
-  if (e.keyCode == 87) keyboard.W = false;
-  if (e.keyCode == 65) keyboard.A = false;  
-  if (e.keyCode == 68) keyboard.D = false;  
-  if (e.keyCode == 32) keyboard.SPACE = false;
-});
-
 function fullscreen(method) {
   if (method === "enter") {
     let fullscreenRef = document.getElementById("fullscreen");
