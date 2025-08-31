98c04042a411a8ba460b11c785b65d4301f0b3a4
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-08-31 16:33:53 +0200

    Added documentation with JsDoc for chicken-small.class

--- classes/chicken-small.class.js
+++ classes/chicken-small.class.js
@@ -1,3 +1,7 @@
+/**
+ * Represents a chicken-small object that can be rendered in the game world.
+ * Inherits movement behavior from {@link MoveableObject}.
+ */
 class ChickenSmall extends MoveableObject {
   static itemCount = 0;
   y = 365;
@@ -20,10 +24,16 @@ class ChickenSmall extends MoveableObject {
     " ./assets/img/3_enemies_chicken/chicken_small/1_walk/3_w.png",
   ];
 
-  IMAGE_DEAD = [
-    "./assets/img/3_enemies_chicken/chicken_small/2_dead/dead.png",
-  ];
+  IMAGE_DEAD = ["./assets/img/3_enemies_chicken/chicken_small/2_dead/dead.png"];
 
+  /**
+   * Creates a new chicken-small object.
+   *
+   * - Loads the walking and dead images.
+   * - Randomly calculates the initial X position within the given parameters.
+   * - Assigns a random movement speed between 0.15 and 0.40.
+   * - Starts the animation cycle.
+   */
   constructor() {
     super().loadImage(this.IMAGE_WALKING[0]);
     this.loadImages(this.IMAGE_WALKING);
@@ -33,6 +43,14 @@ class ChickenSmall extends MoveableObject {
     this.speed = 0.15 + Math.random() * 0.25;
     this.animate();
   }
+
+  /**
+   * Animates the small chicken.
+   *
+   * - If the chicken is not dead:
+   *   - Moves left continuously at ~30 FPS (`1000 / 30 ms`).
+   *   - Cycles through walking animations every 130 ms.
+   */
   animate() {
     if (!this.isDead) {
       this.moveInterval = setInterval(() => {
@@ -45,6 +63,13 @@ class ChickenSmall extends MoveableObject {
     }
   }
 
+  /**
+   * Handles the behavior when the chicken is hit.
+   *
+   * - Sets the `isDead` flag to true.
+   * - Clears the movement and walking intervals.
+   * - Starts a new interval that plays the dead animation every 100 ms.
+   */
   onHit() {
     this.isDead = true;
     clearInterval(this.moveInterval);
