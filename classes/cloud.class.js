db9d2196e444cc657cedfe50b7a1d73f2dafeaba
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-09-01 14:57:53 +0200

    Added documentation with JsDoc for cloud.class

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -1,9 +1,20 @@
+/**
+ * Represents a cloud object that can be rendered in the game world.
+ * Inherits movement behavior from {@link MoveableObject}.
+ */
 class Cloud extends MoveableObject {
   static itemCount = 0;
   y = 20;
   width = 500;
   height = 250;
 
+  /**
+   * Creates a new cloud object.
+   *
+   * - Loads the cloud image
+   * - Randomly calculates the initial X position within the given parameters.
+   * - Starts the animation cycle.
+   */
   constructor(imagePath) {
     super().loadImage(imagePath);
 
@@ -11,6 +22,11 @@ class Cloud extends MoveableObject {
     this.animate();
   }
 
+  /**
+   * Animates the cloud.
+   *
+   * - Moves left continuously at ~60 FPS (`1000 / 60 ms`).
+   */
   animate() {
     setInterval(() => {
       this.moveLeft();
