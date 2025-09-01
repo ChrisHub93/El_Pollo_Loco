29c2e1e907fa46527b4276e8de11d095335e8a7c
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-09-01 16:40:47 +0200

    Added documentation with JsDoc for level.class

--- classes/level.class.js
+++ classes/level.class.js
@@ -1,3 +1,6 @@
+/**
+ * Represents a game level with enemies, clouds, background layers, and collectibles.
+ */
 class Level {
   enemies;
   clouds;
@@ -6,6 +9,15 @@ class Level {
   bottles;
   level_end_x = 2900;
 
+  /**
+   * Creates a new Level instance.
+   *
+   * @param {Array<Enemy>} enemies - The enemies present in the level.
+   * @param {Array<Cloud>} clouds - The clouds used for parallax/background effects.
+   * @param {Array<BackgroundObject>} backgroundObjects - The layered background images.
+   * @param {Array<Coin>} coins - The collectible coins in the level.
+   * @param {Array<Bottle>} bottles - The collectible bottles in the level.
+   */
   constructor(enemies, clouds, backgroundObjects, coins, bottles) {
     this.enemies = enemies;
     this.clouds = clouds;
