78de50a6e39dbef2068449632480bfcb45acc5c5
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-09-01 15:03:00 +0200

    Added documentation with JsDoc for throwable-object.class

--- classes/throwable-object.class.js
+++ classes/throwable-object.class.js
@@ -1,8 +1,12 @@
+/**
+ * Represents a throwable object that can be rendered in the game world.
+ * Inherits movement behavior from {@link MoveableObject}.
+ */
 class ThrowableObject extends MoveableObject {
   speedY = 30;
   speedX = 20;
   rotationInterval = null;
-  hasHit = false; 
+  hasHit = false;
 
   IMAGES_BOTTLE_ROTATION = [
     "./assets/img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
@@ -20,6 +24,15 @@ class ThrowableObject extends MoveableObject {
     "./assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png",
   ];
 
+  /**
+   * Creates a new ThrowableObject (bottle) at the specified coordinates.
+   *
+   * Loads the bottle image, rotation frames, and splash animation frames.
+   * Initializes position and dimensions, then starts the throw animation.
+   *
+   * @param {number} x - The initial x-coordinate of the bottle.
+   * @param {number} y - The initial y-coordinate of the bottle.
+   */
   constructor(x, y) {
     super().loadImage(
       "./assets/img/7_statusbars/3_icons/icon_salsa_bottle.png"
@@ -33,32 +46,51 @@ class ThrowableObject extends MoveableObject {
     this.throw();
   }
 
+  /**
+   * Starts the bottle's throw animation.
+   *
+   * - Applies gravity to the bottle.
+   * - Moves the bottle forward horizontally at a constant speed.
+   * - Plays the rotation animation frames.
+   * - Plays the throw sound if sound is enabled.
+   * - Checks if the bottle hits the ground and triggers the splash animation at y = 370.
+   */
   throw() {
     this.applyGravity();
-  
+
     this.rotationInterval = setInterval(() => {
       this.x += 10;
       this.playAnimation(this.IMAGES_BOTTLE_ROTATION);
       if (soundOn) AUDIO_THROW.play();
-  
+
       if (this.groundHit()) {
         this.splashAnimation(370);
       }
     }, 25);
   }
 
-  groundHit(){
-    return this.y >= 300
+  /**
+   * Checks if the bottle has hit the ground.
+   *
+   * @returns {boolean} True if the bottle hits the ground, otherwise false.
+   */
+  groundHit() {
+    return this.y >= 300;
   }
-  
+
+  /**
+   * Plays the splash animation when the bottle hits the ground or an enemy.
+   * Stops the rotation animation if it is active.
+   *
+   * @param {number} y - The y-coordinate where the splash animation should occur.
+   */
   splashAnimation(y) {
     this.y = y;
     this.playAnimation(this.IMAGES_BOTTLE_SPLASH);
-  
+
     if (this.rotationInterval) {
       clearInterval(this.rotationInterval);
       this.rotationInterval = null;
     }
   }
-  
 }
