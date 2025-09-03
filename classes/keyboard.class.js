2d0d6df9ca55da923433bff18492ce73129a6385
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-09-03 17:28:18 +0200

    Added preventDefault for touch buttons

--- classes/keyboard.class.js
+++ classes/keyboard.class.js
@@ -3,26 +3,51 @@
  * Tracks key states for controlling the character and other actions.
  */
 class Keyboard {
+  keyboardReady = true;
+  W = false;
+  A = false;
+  D = false;
+  B = false;
+  IDLE = false;
+  LONG_IDLE = false;
+
+  idleTimeout = null;
+  longIdleTimeout = null;
+
+  touchButtons = [
+    "touchBtnUp",
+    "touchBtnLeft",
+    "touchBtnRight",
+    "touchBtnThrow",
+  ];
+
   /**
    * Initializes a new instance of the Keyboard class.
    * Sets up default key states, idle timers, and event listeners for keyboard and touch input.
    */
   constructor() {
-    this.keyboardReady = true;
-    this.W = false;
-    this.A = false;
-    this.D = false;
-    this.B = false;
-    this.IDLE = false;
-    this.LONG_IDLE = false;
-
-    this.idleTimeout = null;
-    this.longIdleTimeout = null;
-
     window.addEventListener("keydown", (e) => this.handleKey(e, true));
     window.addEventListener("keyup", (e) => this.handleKey(e, false));
-    window.addEventListener("touchstart", (e) => this.handleKey(e, true));
-    window.addEventListener("touchend", (e) => this.handleKey(e, false));
+
+    this.touchButtons.forEach((id) => {
+      const btn = document.getElementById(id);
+      if (!btn) return;
+
+      btn.addEventListener(
+        "touchstart",
+        (e) => {
+          e.preventDefault();
+          this.handleKey(e, true);
+        },
+        { passive: false }
+      );
+
+      btn.addEventListener("touchend", (e) => {
+        e.preventDefault();
+        this.handleKey(e, false);
+      });
+    });
+
     this.setIdle(this.D, true);
   }
 
@@ -33,17 +58,17 @@ class Keyboard {
    * @param {boolean} isPressed - True if the key or touch is pressed, false if released.
    */
   handleKey(e, isPressed) {
-    const key = e.key; 
-  
+    const key = e.key;
+
     if (key === "w" || key === "W" || e.target.id === "touchBtnUp")
       this.setIdle("W", isPressed);
-  
+
     if (key === "a" || key === "A" || e.target.id === "touchBtnLeft")
       this.setIdle("A", isPressed);
-  
+
     if (key === "d" || key === "D" || e.target.id === "touchBtnRight")
       this.setIdle("D", isPressed);
-  
+
     if (key === "b" || key === "B" || e.target.id === "touchBtnThrow")
       this.setIdle("B", isPressed);
   }
